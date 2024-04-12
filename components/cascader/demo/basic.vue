2d84e6fdc0c2d5a1e7fce20e89008b7b489009a6
makck56 <markcokoeng@gmail.com>
2024-04-12 14:13:55 +0800

    first commit

---
+++ components/cascader/demo/basic.vue
@@ -0,0 +1,68 @@
+<docs>
+---
+order: 0
+title:
+  zh-CN: 基本用法
+  en-US: Basic
+---
+
+## zh-CN
+
+省市区级联。
+
+## en-US
+
+Cascade selection box for selecting province/city/district.
+
+</docs>
+<template>
+  <a-cascader v-model:value="value" :options="options" placeholder="Please select" >
+    <template #option="option">{{  option.label }}+++</template>
+  </a-cascader>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import type { CascaderProps } from 'ant-design-vue';
+const options: CascaderProps['options'] = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+export default defineComponent({
+  setup() {
+    return {
+      value: ref<string[]>([]),
+      options,
+    };
+  },
+});
+</script>
---
+++ components/select/demo/custom-dropdown-menu.vue
@@ -0,0 +1,62 @@
+<docs>
+---
+order: 13
+title:
+  zh-CN: 扩展菜单
+  en-US: Custom dropdown
+---
+
+## zh-CN
+
+使用 `dropdownRender` 对下拉菜单进行自由扩展。
+
+## en-US
+
+Customize the dropdown menu via `dropdownRender`.
+
+</docs>
+
+<template>
+  <a-select
+    v-model:value="value"
+    style="width: 120px"
+    :options="items.map(item => ({ value: item }))"
+  >
+    <template #dropdownRender="{ menuNode: menu }">
+      <v-nodes :vnodes="menu" />
+      <a-divider style="margin: 4px 0" />
+      <a-input
+        style="padding: 4px 8px; cursor: pointer"
+        @click="addItem"
+      >
+      </a-input>
+    </template>
+  </a-select>
+</template>
+<script lang="ts">
+import { PlusOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+
+let index = 0;
+export default defineComponent({
+  components: {
+    PlusOutlined,
+    VNodes: (_, { attrs }) => {
+      return attrs.vnodes;
+    },
+  },
+  setup() {
+    const items = ref(['jack', 'lucy']);
+    const value = ref('lucy');
+
+    const addItem = () => {
+      console.log('addItem');
+    };
+    return {
+      items,
+      value,
+      addItem,
+    };
+  },
+});
+</script>
